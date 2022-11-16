333441e4ae10841876b3ce1eb3428343c6c0b8fe
Dirie Hersi <dnhersi@gmail.com>
2022-11-16 22:04:22 +0000

    Fixed variable aws_region

--- networking/main.tf
+++ networking/main.tf
@@ -8,7 +8,7 @@ resource "random_integer" "random" {
 resource "aws_vpc" "terraform_dnd" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
-  enable_dns_support  = true
+  enable_dns_support   = true
 
   tags = {
     Name = "terraform_dnd-${random_integer.random.id}"
--- networking/outputs.tf
+++ networking/outputs.tf
@@ -1,5 +1,5 @@
 # --- networking/outputs.tf --- 
 
 output "vpc_id" {
-    value = aws_vpc.terraform_dnd.id
+  value = aws_vpc.terraform_dnd.id
 }
