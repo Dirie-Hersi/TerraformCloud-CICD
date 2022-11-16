bc9e168dbeffe9bd590476f6a89be72defac4744
Dirie Hersi <dnhersi@gmail.com>
2022-11-16 23:59:41 +0000

    temporarily commented out vpc

--- networking/main.tf
+++ networking/main.tf
@@ -1,16 +1,16 @@
 #---networking/main.tf ---
 
-resource "random_integer" "random" {
-  min = 1
-  max = 100
-}
+# resource "random_integer" "random" {
+#   min = 1
+#   max = 100
+# }
 
-resource "aws_vpc" "terraform_dnd" {
-  cidr_block           = var.vpc_cidr
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+# resource "aws_vpc" "terraform_dnd" {
+#   cidr_block           = var.vpc_cidr
+#   enable_dns_hostnames = true
+#   enable_dns_support   = true
 
-  tags = {
-    Name = "terraform_dnd-${random_integer.random.id}"
-  }
-}
+#   tags = {
+#     Name = "terraform_dnd-${random_integer.random.id}"
+#   }
+# }
